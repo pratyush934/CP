1f5bedb4bac91d95a94765cefc00c6983d94d539
Pratyush <oggycandle786@gmail.com>
2024-06-27 22:52:04 +0530

    Adding relevant things

--- src/MacroTemplates.java
+++ src/MacroTemplates.java
@@ -16,23 +16,165 @@ import java.math.*;
 
 public class MacroTemplates {
 
-    public static void main(String[] hi) throws Exception {
-        BufferedReader infile = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(infile.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        /*
-        int T = Integer.parseInt(st.nextToken());
-        StringBuilder sb = new StringBuilder();
-        while(T-->0)
-        {
-            st = new StringTokenizer(infile.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int[] arr = readArr(N, infile, st);
+    static class FastScanner {
+        //I don't understand how this works lmao
+        private int BS = 1 << 16;
+        private char NC = (char) 0;
+        private byte[] buf = new byte[BS];
+        private int bId = 0, size = 0;
+        private char c = NC;
+        private double cnt = 1;
+        private BufferedInputStream in;
+
+        public FastScanner() {
+            in = new BufferedInputStream(System.in, BS);
         }
-        */
-        //BufferedReader infile = new BufferedReader(new FileReader("input.txt"));
-        //System.setOut(new PrintStream(new File("output.txt")));
+
+        public FastScanner(String s) {
+            try {
+                in = new BufferedInputStream(new FileInputStream(new File(s)), BS);
+            } catch (Exception e) {
+                in = new BufferedInputStream(System.in, BS);
+            }
+        }
+
+        private char getChar() {
+            while (bId == size) {
+                try {
+                    size = in.read(buf);
+                } catch (Exception e) {
+                    return NC;
+                }
+                if (size == -1) return NC;
+                bId = 0;
+            }
+            return (char) buf[bId++];
+        }
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public int[] nextInts(int N) {
+            int[] res = new int[N];
+            for (int i = 0; i < N; i++) {
+                res[i] = (int) nextLong();
+            }
+            return res;
+        }
+
+        public long[] nextLongs(int N) {
+            long[] res = new long[N];
+            for (int i = 0; i < N; i++) {
+                res[i] = nextLong();
+            }
+            return res;
+        }
+
+        public long nextLong() {
+            cnt = 1;
+            boolean neg = false;
+            if (c == NC) c = getChar();
+            for (; (c < '0' || c > '9'); c = getChar()) {
+                if (c == '-') neg = true;
+            }
+            long res = 0;
+            for (; c >= '0' && c <= '9'; c = getChar()) {
+                res = (res << 3) + (res << 1) + c - '0';
+                cnt *= 10;
+            }
+            return neg ? -res : res;
+        }
+
+        public double nextDouble() {
+            double cur = nextLong();
+            return c != '.' ? cur : cur + nextLong() / cnt;
+        }
+
+        public double[] nextDoubles(int N) {
+            double[] res = new double[N];
+            for (int i = 0; i < N; i++) {
+                res[i] = nextDouble();
+            }
+            return res;
+        }
+
+        public String next() {
+            StringBuilder res = new StringBuilder();
+            while (c <= 32) c = getChar();
+            while (c > 32) {
+                res.append(c);
+                c = getChar();
+            }
+            return res.toString();
+        }
+
+        public String nextLine() {
+            StringBuilder res = new StringBuilder();
+            while (c <= 32) c = getChar();
+            while (c != '\n') {
+                res.append(c);
+                c = getChar();
+            }
+            return res.toString();
+        }
+
+        public boolean hasNext() {
+            if (c > 32) return true;
+            while (true) {
+                c = getChar();
+                if (c == NC) return false;
+                else if (c > 32) return true;
+            }
+        }
+    }
+
+    static class FastOutput {
+        private final BufferedWriter bw;
+
+        public FastOutput() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+    public static void main(String[] hi) throws Exception {
+
+         try {
+
+             FastScanner in = new FastScanner();
+             FastOutput out = new FastOutput();
+
+             int T = in.nextInt();
+
+             while(T-- > 0) {
+                 //code
+                 int a = in.nextInt();
+                 int b = in.nextInt();
+
+                 String str = in.nextLine();
+
+                 out.println(a+b);
+                 out.println(str);
+             }
+
+             out.close();
+
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
     }
 
     public static int[] readArr(int N, BufferedReader infile, StringTokenizer st) throws Exception {
@@ -680,116 +822,5 @@ class MaxFlow {
     }
 }
 
-class FastScanner {
-    //I don't understand how this works lmao
-    private int BS = 1 << 16;
-    private char NC = (char) 0;
-    private byte[] buf = new byte[BS];
-    private int bId = 0, size = 0;
-    private char c = NC;
-    private double cnt = 1;
-    private BufferedInputStream in;
 
-    public FastScanner() {
-        in = new BufferedInputStream(System.in, BS);
-    }
-
-    public FastScanner(String s) {
-        try {
-            in = new BufferedInputStream(new FileInputStream(new File(s)), BS);
-        } catch (Exception e) {
-            in = new BufferedInputStream(System.in, BS);
-        }
-    }
-
-    private char getChar() {
-        while (bId == size) {
-            try {
-                size = in.read(buf);
-            } catch (Exception e) {
-                return NC;
-            }
-            if (size == -1) return NC;
-            bId = 0;
-        }
-        return (char) buf[bId++];
-    }
-
-    public int nextInt() {
-        return (int) nextLong();
-    }
-
-    public int[] nextInts(int N) {
-        int[] res = new int[N];
-        for (int i = 0; i < N; i++) {
-            res[i] = (int) nextLong();
-        }
-        return res;
-    }
-
-    public long[] nextLongs(int N) {
-        long[] res = new long[N];
-        for (int i = 0; i < N; i++) {
-            res[i] = nextLong();
-        }
-        return res;
-    }
-
-    public long nextLong() {
-        cnt = 1;
-        boolean neg = false;
-        if (c == NC) c = getChar();
-        for (; (c < '0' || c > '9'); c = getChar()) {
-            if (c == '-') neg = true;
-        }
-        long res = 0;
-        for (; c >= '0' && c <= '9'; c = getChar()) {
-            res = (res << 3) + (res << 1) + c - '0';
-            cnt *= 10;
-        }
-        return neg ? -res : res;
-    }
-
-    public double nextDouble() {
-        double cur = nextLong();
-        return c != '.' ? cur : cur + nextLong() / cnt;
-    }
-
-    public double[] nextDoubles(int N) {
-        double[] res = new double[N];
-        for (int i = 0; i < N; i++) {
-            res[i] = nextDouble();
-        }
-        return res;
-    }
-
-    public String next() {
-        StringBuilder res = new StringBuilder();
-        while (c <= 32) c = getChar();
-        while (c > 32) {
-            res.append(c);
-            c = getChar();
-        }
-        return res.toString();
-    }
-
-    public String nextLine() {
-        StringBuilder res = new StringBuilder();
-        while (c <= 32) c = getChar();
-        while (c != '\n') {
-            res.append(c);
-            c = getChar();
-        }
-        return res.toString();
-    }
-
-    public boolean hasNext() {
-        if (c > 32) return true;
-        while (true) {
-            c = getChar();
-            if (c == NC) return false;
-            else if (c > 32) return true;
-        }
-    }
-}
 //use for codeforces!
